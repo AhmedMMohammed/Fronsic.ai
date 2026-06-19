60be10743b22cd507dc049cedb4c1e4402961630
Aquder <anaahmedmohamed49@gmail.com>
2026-06-20 00:32:58 +0300

    fix: update __DIR__ constants and bind uppercase App path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+// 1. وضع التطبيق في متغير بدلاً من عمل return مباشر
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -20,3 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// 2. إجبار لارافيل على قراءة مجلد App بالحرف الكبير
+$app->useAppPath($app->basePath('App'));
+
+// 3. إرجاع التطبيق بعد التعديل
+return $app;
